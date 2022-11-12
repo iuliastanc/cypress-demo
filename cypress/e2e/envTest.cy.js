86611eed4fdfcde1d11c6611ad30f6d3d2acc7f0
Iulia Stanc <iulia.stanc-ext@power.cloud>
2022-11-12 13:41:28 +0200

    env practice

--- cypress/e2e/envTest.cy.js
+++ cypress/e2e/envTest.cy.js
@@ -2,7 +2,9 @@ const SimpleCalculatorPage = require('../support/pages/SimpleCalculator');
 
 describe.only('Env test', () => {
   it('should have correct env', () => {
-    const env = Cypress.env();
-    cy.logToTerminal(JSON.stringify(env, null, 2));
+    const x = Cypress.config('x');
+    cy.logToTerminal(env);
+    const currentEnv = Cypress.env[env];
+    cy.logToTerminal(JSON.stringify(currentEnv, null, 2));
   });
 });
